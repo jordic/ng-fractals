8d9ecb9197dfefb5c4d66031c9809899e5cc3e45
Jordi Collell <jordic@gmail.com>
2016-12-25 20:11:42 +0100

    node rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,9 @@ import { scaleLinear } from 'd3-scale';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
@@ -16,6 +18,19 @@ import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/combineLatest';
 
 import { animationFrame } from 'rxjs/scheduler/animationFrame';
+
+export function filter(x: MouseEvent, y: MouseEvent): boolean {
+  const dx = Math.abs(x.offsetX - y.offsetX);
+  const dy = Math.abs(x.offsetY - y.offsetY);
+  return ((dx + dy) >= 10 ? false : true);
+}
+
+/*
+(x: MouseEvent, y: MouseEvent) => {
+          return (Math.round(x.offsetX) === Math.round(y.offsetX) ||
+            Math.round(x.offsetY) === Math.round(y.offsetY));
+        }
+*/
 
 @Component({
   selector: 'app-root',
@@ -43,20 +58,17 @@ export class AppComponent implements OnInit {
     this.zone.runOutsideAngular(() => {
       this.stream$ = Observable
         .fromEvent(this.svgRef.nativeElement, 'mousemove')
-        .distinctUntilChanged((x: MouseEvent, y: MouseEvent) => {
-          return (Math.round(x.offsetX) === Math.round(y.offsetX) ||
-            Math.round(x.offsetY) === Math.round(y.offsetY));
-        })
-        // .debounceTime(2)
-        .map((mouseEvent: MouseEvent) => {
+        .distinctUntilChanged(filter)
+        .debounceTime(2)
+        .switchMap((mouseEvent: MouseEvent) => {
           const { offsetX: x, offsetY: y } = mouseEvent;
           const scaleFactor = scaleLinear().domain([this.height, 0]).range([0, .8]);
           const scaleLean = scaleLinear()
             .domain([0, this.width / 2, this.width]).range([.5, 0, -.5]);
-          return {
+          return Observable.of({
             heightFactor: scaleFactor(y),
             lean: scaleLean(x)
-          };
+          });
         })
         .startWith({ heightFactor: 0, lean: 0 })
         .combineLatest(Observable.interval(500).take(this.realMax))
